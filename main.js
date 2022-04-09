2bc09c0938501a4cf559f5d54182eea9db9a1db0
UncomfortableChai <mj1055119@gmail.com>
2022-04-09 01:05:47 -0700

    Changed the style

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const speechBubble = document.querySelector("#text");
 const btn = document.querySelector("#submitBtn");
 
+var msg = new SpeechSynthesisUtterance();
+msg.rate = 1.2;
+msg.pitch = 2;
 btn.addEventListener("click", () => {
     txt = speechBubble.value;
     console.log(txt);
 
-    var msg = new SpeechSynthesisUtterance();
-    msg.text = txt;
+    msg.text = `${txt}`;
     window.speechSynthesis.speak(msg);
 
-    txt = " ";
+    setTimeout(clearText(), 8000);
+    console.log(txt);
+
 });
+
+const clearText = () => { txt = " "; };
