940894621ce71a1da6088ce19ee3219d2cbadc30
UncomfortableChai <mj1055119@gmail.com>
2022-05-19 15:31:34 -0700

    Fixed Load after button submit

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const speechBubble = document.querySelector("#text");
 const btn = document.querySelector("#submitBtn");
-
+const select = document.querySelector("#language");
+const opt = select.getElementsByTagName("option");
 var msg = new SpeechSynthesisUtterance();
 msg.rate = 1.2;
 msg.pitch = 2;
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
+    speek();
+    e.preventDefault();
+})
+
+function speek() {
     txt = speechBubble.value;
     console.log(txt);
-
     msg.text = `${txt}`;
     window.speechSynthesis.speak(msg);
-
-    setTimeout(clearText(), 8000);
-    console.log(txt);
-
-});
-
-const clearText = () => { txt = " "; };
+    speechBubble.value = "";
+}
