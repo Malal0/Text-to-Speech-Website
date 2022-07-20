169b59248e263e19e70c1c18b7056c0830a76b3e
UncomfortableChai <mj1055119@gmail.com>
2022-07-19 22:26:17 -0700

    added a delete text btn html element

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const select = document.querySelector("#language");
 const pitch = document.querySelector("#pitch");
 const rate = document.querySelector("#rate");
 const volume = document.querySelector("#volume");
-//const opt = select.getElementsByTagName("option");
 const pitchInputText = document.querySelector("#pitch-input-text");
 var msg = new SpeechSynthesisUtterance();
 msg.rate = 1.2;
@@ -19,7 +18,6 @@ btn.addEventListener("click", (e) => {
 pitch.addEventListener("change", () => {
     msg.pitch = pitch.value;
     pitchInputText.value = pitch.value;
-    console.log(pitch.value);
 })
 
 rate.addEventListener("change", () => {
@@ -32,15 +30,18 @@ volume.addEventListener("change", () => {
     console.log(volume.value);
 })
 
+pitch.addEventListener("mousedown", () => {
+    msg.pitch = pitch.value;
+    pitchInputText.value = pitch.value;
+})
+
 pitchInputText.addEventListener("change", () => {
     msg.pitch = pitchInputText.value;
     pitch.value = pitchInputText.value;
-    console.log(pitch.value);
 })
 
 function speak() {
     txt = speechBubble.value;
-    console.log(txt);
 
     msg.text = `${txt}`;
     window.speechSynthesis.speak(msg);
