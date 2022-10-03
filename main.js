ccbacde6c4106a138a903196b2ddce252555d08e
UncomfortableChai <mj1055119@gmail.com>
2022-10-02 17:06:13 -0700

    added a function to populate voices

--- main.js
+++ main.js
@@ -78,5 +78,32 @@ function speak() {
 }
 
 function changeLanguage() {
-    msg.lang = select.value
+    msg.lang = select.selectedOptions[0].getAttribute('data-lang');
+    console.log(select.selectedOptions);
+}
+
+function populateVoiceList() {
+    if (typeof speechSynthesis === 'undefined') {
+        return;
+    }
+
+    const voices = speechSynthesis.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+        const option = document.createElement('option');
+        option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+        if (voices[i].default) {
+            option.textContent += ' — DEFAULT';
+        }
+
+        option.setAttribute('data-lang', voices[i].lang);
+        option.setAttribute('data-name', voices[i].name);
+        select.appendChild(option);
+    }
+}
+
+populateVoiceList();
+if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
 }
